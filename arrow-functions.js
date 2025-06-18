1bf979d7f01962ef97b63dac8ba067ca547ff75a
Zozo62442 <zoe.rotterr@gmail.com>
2025-06-18 15:36:16 +0200

    learn fetch and API

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,8 +1,3 @@
-/**
- * To run this file in Gitpod, use the 
- * command node arrow-functions.js in the terminal
- */
-
 // Vanilla JavaScript Function
 // function addTwoNumbers(a, b) {
 //     // Code block
